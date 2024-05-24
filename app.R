30bc112065795969a8b554b97202bb2584705d8e
Adrian Curia <adrian.curia@linkhipu.com>
2024-05-24 12:48:23 +0300

    new

--- app.R
+++ app.R
@@ -1,72 +1,55 @@
+install.packages("shinydashboard")
+
 library(shiny)
 library(dplyr)
 library(ggplot2)
 library(gapminder)
+library(shinydashboard)
 
 # Specify the application port
 options(shiny.host = "0.0.0.0")
 options(shiny.port = 8180)
 
+# UI
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+        fluidRow(
+          box(plotOutput("plot1", height = 250)),
 
-ui <- fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      tags$h4("Gapminder Dashboard"),
-      tags$hr(),
-      selectInput(inputId = "inContinent", label = "Continent", choices = unique(gapminder$continent), selected = "Europe")
-    ),
-    mainPanel(
-      plotOutput(outputId = "outChartLifeExp"),
-      plotOutput(outputId = "outChartGDP")
+          box(
+            title = "Controls",
+            sliderInput("slider", "Number of observations:", 1, 100, 50)
+          )
+        )
+      ),
+
+      # Second tab content
+      tabItem(tabName = "widgets",
+        h2("Widgets tab content")
+      )
     )
   )
 )
 
-server <- function(input, output, session) {
-  # Filter data and store as reactive value
-  data <- reactive({
-    gapminder %>%
-      filter(continent == input$inContinent) %>%
-      group_by(year) %>%
-      summarise(
-        AvgLifeExp = round(mean(lifeExp)),
-        AvgGdpPercap = round(mean(gdpPercap), digits = 2)
-      )
-  })
+# Server 
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
 
-  # Common properties for charts
-  chart_theme <- ggplot2::theme(
-    plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
-    axis.title.x = element_text(size = 15),
-    axis.title.y = element_text(size = 15),
-    axis.text.x = element_text(size = 12),
-    axis.text.y = element_text(size = 12)
-  )
-
-  # Render Life Exp chart
-  output$outChartLifeExp <- renderPlot({
-    ggplot(data(), aes(x = year, y = AvgLifeExp)) +
-      geom_col(fill = "#0099f9") +
-      geom_text(aes(label = AvgLifeExp), vjust = 2, size = 6, color = "#ffffff") +
-      labs(title = paste("Average life expectancy in", input$inContinent)) +
-      theme_classic() +
-      chart_theme
-  })
-
-  # Render GDP chart
-  output$outChartGDP <- renderPlot({
-    ggplot(data(), aes(x = year, y = AvgGdpPercap)) +
-      geom_line(color = "#f96000", size = 2) +
-      geom_point(color = "#f96000", size = 5) +
-      geom_label(
-        aes(label = AvgGdpPercap),
-        nudge_x = 0.25,
-        nudge_y = 0.25
-      ) +
-      labs(title = paste("Average GDP per capita in", input$inContinent)) +
-      theme_classic() +
-      chart_theme
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
   })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
